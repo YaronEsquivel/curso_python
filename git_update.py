01b2faef8018e6996d92728f2e8817bca4b4f30f
YaronEsquivel <yaron.esquivel@axity.com>
2026-04-12 17:59:38 -0600

    automatizando actualizacion de git

--- git_update.py
+++ git_update.py
@@ -3,3 +3,4 @@ import subprocess
 subprocess.run("git add .", shell=True)
 message = input(" agregar mensaje para el commit > ")
 subprocess.run(["git", "commit", "-m", message])
+subprocess.run(["git", "push"])
